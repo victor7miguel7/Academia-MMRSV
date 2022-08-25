40f8cc72f00bfee424d90138e7323ec2513d5a8b
Stella <stellanacleto@gmail.com>
2022-08-25 17:38:36 -0300

    Commit inicial

---
+++ Academia/src/Usuario.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+
+public class Usuario {
+	
+	String nome;
+	String email;
+	String senha;
+	LocalDate dtNascimento;
+	
+	public Usuario(String nome, String email, String senha, LocalDate dtNascimento) {
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.dtNascimento = dtNascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public LocalDate getDtNascimento() {
+		return dtNascimento;
+	}
+	
+	public void cadastrarLogin (String Login, String senha) {
+		
+	}
+
+}
