8b6947a40923940aae6c88304321edfb04e6b87e
Rafael Neto <rafaelneto686@gmail.com>
2022-08-30 15:04:08 -0300

    Atualização do RepositorioGenerico.

--- Academia/src/dados/RepositorioGenerico.java
+++ Academia/src/dados/RepositorioGenerico.java
@@ -1,35 +1,47 @@
 package dados;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class RepositorioGenerico<R> implements IRepositorioGenerico {
+public class RepositorioGenerico<R> implements IRepositorioGenerico<R> {
 
 	protected List<R> elementos;
 
 	public RepositorioGenerico() {
 		this.elementos = new ArrayList<>();
-		
-		if (elementos != null && elementos instanceof List<?>){
-            this.elementos = (List<R>) elementos;
-        }
+
+		if (elementos != null && elementos instanceof List<?>) {
+			this.elementos = (List<R>) elementos;
+		}
 	}
 
-	public void inserir(Object novoObj) {
+
+	public void inserir(R novoObj) {
+		if (!this.elementos.contains(novoObj)) {
+			this.elementos.add( novoObj);
+		}
 
 	}
 
-	public void remover(Object obj) {
+	public void remover(R obj) {
+		if(this.elementos.contains(obj)) {
+			this.elementos.remove(this.elementos.indexOf(obj));
+		}
 
 	}
 
-	public void atualizar(Object newObj) {
+
+	public void atualizar(R newObj) {
+		if (this.elementos.contains(newObj)) {
+			int indice = this.elementos.indexOf(newObj);
+			this.elementos.set(indice,newObj);
+		}
 
 	}
 
 	public List<R> listar() {
 
-		return null;
+		return Collections.unmodifiableList(this.elementos);
 	}
-
 }
