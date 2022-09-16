ba1b41169ba7def201d3b284292c6336ffeeb15e
Victor Miguel <victor78miguel@gmail.com>
2022-09-16 12:42:40 -0300

    teste

--- Academia/src/models/Administrador.java
+++ Academia/src/models/Administrador.java
@@ -6,7 +6,7 @@ public class Administrador extends Usuario{
 
 	public Administrador(String id, String nome, Login login, LocalDate dtNascimento) {
 		super(id, nome, login, dtNascimento);
-		
+		//testandoo
 	}
 	
 }
